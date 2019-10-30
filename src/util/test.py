d42b790e731755a273756847a9ee37458d9059a4
Veronicacz <35347214+Veronicacz@users.noreply.github.com>
2019-10-30 16:36:31 -0400

    Add files via upload

---
+++ src/util/test.py
@@ -0,0 +1,16 @@
+import os
+import json
+import mujoco_py
+
+# ab = '/home/veronica/Desktop/Deep-Reinforcement-Learning-in-Large-Discrete-Action-Spaces-master/results/obj/'
+# cd = 'temp/0'
+# de = 'data_2500_Wolp3_Inv1000k100#0'
+
+# name = ab + cd + de + '.json'
+# with open(name, 'w') as f:
+# 	print(name)
+# 	json.dump("self.data", f)
+
+
+
+print(mj_version())
